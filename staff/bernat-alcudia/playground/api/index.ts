8fff3dc1119cc7d791e08ef287c07faa2abf6a15
Bernat Alcudia <bernatalcudia@gmail.com>
2025-07-08 17:09:23 +0200

    build: Implement initial code #9

---
+++ staff/bernat-alcudia/playground/api/index.ts
@@ -0,0 +1,35 @@
+type User = {
+    id: string
+    name: string
+    email: string
+    username: string
+    password: string
+}
+
+type Post = {
+    id: string
+    author: string
+    image: string
+    text: string
+    date: Date
+}
+
+const post1: Post = { id: "1", author: "1", image: "https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExNjEwbTRxZDIybXEybmR0OXMyYmljdmhxYjNpenl3eXl0Y3EwYmdtdiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/lDR0wnXboVr8c/giphy.gif", text: "taking a nap", date: new Date() }
+
+const peter: User = { id: "1", name: "Peter Pan", email: "peter@pan.com", username: "peterpan", password: "123123123" }
+const wendy: User = { id: "2", name: "Wendy Darling", email: "wendy@darling.com", username: "wendydarling", password: "123123123" }
+const pepito: User = { id: "3", name: "Pepito Grillo", email: "pepito@grillo.com", username: "pepitogrillo", password: "123123123" }
+const campa: User = { id: "4", name: "Campa Nilla", email: "campa@nilla.com", username: "campanilla", password: "123123123" }
+
+const users: User[] = []
+const posts: Post[] = []
+
+users.push(peter)
+users.push(wendy)
+users.push(pepito)
+users.push(campa)
+
+posts.push(post1)
+
+console.table(users)
+console.table(posts)
